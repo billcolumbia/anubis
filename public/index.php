fb5960cc9dc58a066d05143efbcf2bd47cab1e67
Bill Columbia <hello@billcolumbia.com>
2020-07-10 20:00:34 -0400

    prototype working

--- public/index.php
+++ public/index.php
@@ -1,1 +1,16 @@
-<h1>Hello I am PHP</h1>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello</title>
+    <link rel="stylesheet" href="/index.css">
+</head>
+
+<body>
+    I AM PHP
+    <div class="b">ded</div>
+</body>
+
+</html>
--- src/machone.js
+++ src/machone.js
@@ -1,17 +1,21 @@
+const path = require('path')
 const chokidar = require('chokidar')
 const httpProxy = require('http-proxy')
 const http = require('http')
-const WebSocket = require('ws')
-const url = require('url')
+const express = require('express')
+const socketio = require('socket.io')
+const connect = require('connect')
+const harmon = require('harmon')
 
 const defaults = {
-  watch: './stuff/**/*.{css,js}'
+  watch: './public/**/*.{css,js}'
 }
 
 const setup = (options = defaults) => {
   MachOne.options = Object.assign({}, options, defaults)
-  MachOne.watch()
-  MachOne.serve()
+  // MachOne.watch()
+  // MachOne.serve()
+  MachOne.proxy()
 }
 
 const watch = () => {
@@ -23,13 +27,72 @@ const watch = () => {
 }
 
 const serve = () => {
-  // MachOne.proxy()
+  const app = express()
+  const server = http.createServer(app)
+  const io = socketio(server)
+
+  app.use(express.static(path.join(__dirname, '../public')))
+
+  let count = 0
+
+  io.on('connection', (socket) => {
+    console.log('Connection established!')
+
+    socket.emit('countUpdated', count)
+
+    socket.on('increment', () => {
+      count++
+      console.log(count)
+      io.emit('countUpdated', count)
+    })
+  })
+
+  server.listen(9000, () => {
+    console.log('http://localhost:9000')
+  })
 }
 
 const proxy = () => {
-  httpProxy.createProxyServer({
+  const selects = [{
+    query: 'body',
+    func (node) {
+      const stuff = `<!-- injected via via MachOne -->
+<script src="/socket.io/socket.io.js"></script>
+<script src="/client.js"></script>
+<!-- /injected via via MachOne -->`
+      const read = node.createReadStream()
+      const write = node.createWriteStream({ outer: false })
+      read.pipe(write, { end: false })
+      read.on('end', () => {
+        write.end(stuff)
+      })
+    }
+  }]
+
+  const app = connect()
+  const proxied = httpProxy.createProxyServer({
     target: 'http://localhost:8080'
-  }).listen('9000')
+  })
+  const server = http.createServer(app)
+  const io = socketio(server)
+  app.use(harmon([], selects))
+  app.use((req, res) => {
+    proxied.web(req, res)
+  })
+  io.on('connection', (socket) => {
+    console.log('Connection established!')
+  })
+
+  chokidar
+    .watch(MachOne.options.watch)
+    .on('all', (event, path) => {
+      console.log(event, path)
+      io.emit('filesUpdated', path)
+    })
+  server.listen(9000)
+  // httpProxy.createProxyServer({
+  //   target: 'http://localhost:8080'
+  // }).listen('9000')
 }
 
 const MachOne = {
