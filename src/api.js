08f76a8c6bf82261688a149ec60375b9bfa6126f
Bill Columbia <hello@billcolumbia.com>
2020-07-18 22:01:14 -0400

    drop harmon, modify request ourselves

--- src/api.js
+++ src/api.js
@@ -4,9 +4,9 @@ const httpProxy = require('http-proxy')
 const http = require('http')
 const socketio = require('socket.io')
 const connect = require('connect')
-const harmon = require('harmon')
 const serveStatic = require('serve-static')
 const finalhandler = require('finalhandler')
+const Buffer = require('buffer').Buffer
 const path = require('path')
 const { log } = console
 const defaults = require('./defaults')
@@ -82,29 +82,46 @@ const Anubis = (userOptions) => {
     }
   }
 
-  const injectClient = (node) => {
-    const read = node.createReadStream()
-    const write = node.createWriteStream({ outer: false })
-    read.pipe(write, { end: false })
-    read.on('end', () => {
-      write.end(scriptsToInject(opts.port))
-    })
-  }
-
   const createServer = () => {
     const app = connect()
     const proxied = httpProxy.createProxyServer({
       target: opts.target
     })
-    app.use(harmon([], [{
-      query: 'body',
-      func: injectClient
-    }]))
     app.use((req, res, next) => {
       if (req.url !== `/${clientScriptName}`) proxied.web(req, res)
       else {
         const serve = serveStatic(path.join(__dirname))
         serve(req, res, finalhandler(req, res))
+      }
+    })
+    proxied.on('proxyRes', function (proxyRes, req, res) {
+      const isHTML = proxyRes.headers['content-type'].indexOf('text/html') > -1
+      if (isHTML) {
+        let markup = null
+        const _write = res.write
+        const _writeHead = res.writeHead
+        const _end = res.end
+
+        res.writeHead = function () {
+          var headers = (arguments.length > 2) ? arguments[2] : arguments[1]
+          headers = headers || {}
+          res.removeHeader('Content-Length')
+          delete headers['content-length']
+          _writeHead.apply(res, arguments)
+        }
+
+        res.write = (data) => {
+          markup = data.toString()
+        }
+
+        res.end = () => {
+          const resBody = markup.replace(
+            '</body>',
+            `${scriptsToInject(opts.port)}\n</body>`
+          )
+          _write.call(res, Buffer.from(resBody))
+          _end.call(res)
+        };
       }
     })
     server = http.createServer(app)
