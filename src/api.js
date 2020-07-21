8947077d3f6f5dd5065143fab176a40ce8b1d3a3
Bill Columbia <hello@billcolumbia.com>
2020-07-21 14:43:08 -0400

    maybe fix res logic

--- src/api.js
+++ src/api.js
@@ -88,6 +88,15 @@ const Anubis = (userOptions) => {
     }
   }
 
+  const injectClient = (node) => {
+    const read = node.createReadStream()
+    const write = node.createWriteStream({ outer: false })
+    read.pipe(write, { end: false })
+    read.on('end', () => {
+      write.end(scriptsToInject(opts.port))
+    })
+  }
+
   /**
    * Overwrite write, writeHead, and end in the request life cycle to modify
    * the response by injecting our socket scripts. This makes it automatic
@@ -98,18 +107,16 @@ const Anubis = (userOptions) => {
    * @param {*} next 
    */
   const injectScripts = (req, res, next) => {
-    let resString = null
-    let resData = null
     const _write = res.write
     const _writeHead = res.writeHead
     const _end = res.end
     const isHTML = (res) => {
-      const contentHeader = res.getHeader('Content-Type')
-      return contentHeader && contentHeader.indexOf('text/html') > -1
+      const contentType = res.getHeader('Content-Type') || ''
+      return contentType.indexOf('text/html') === 0
     }
 
     res.writeHead = function () {
-      let headers = (arguments.length > 2) ? arguments[2] : arguments[1]
+      var headers = (arguments.length > 2) ? arguments[2] : arguments[1]
       headers = headers || {}
       if (isHTML(res)) {
         res.removeHeader('Content-Length')
@@ -118,21 +125,17 @@ const Anubis = (userOptions) => {
       _writeHead.apply(res, arguments)
     }
 
-    res.write = (data) => {
-      resString = data.toString()
-      resData = data
+    res.write = function (data) {
+      if (isHTML(res)) {
+        const resString = data.toString().replace('</body>', `${scriptsToInject(opts.port)}\n</body>`)
+        _write.call(res, Buffer.from(resString))
+      } else _write.apply(res, arguments)
     }
 
-    res.end = () => {
-      if (isHTML(res)) {
-        resString = resString.replace('</body>', `${scriptsToInject(opts.port)}\n</body>`)
-        _write.call(res, Buffer.from(resString))
-      } else {
-        _write.call(res, resData)
-      }
-
+    res.end = function (data, encoding) {
       _end.call(res)
     }
+
     next()
   }
 
